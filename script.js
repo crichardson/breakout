e29843208e8d3c3fd3e24728124fc79f91f36565
crichardson <calebr@scholarsnyc.com>
2013-05-06 10:50:05 -0300

    ctx

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hey, how's it going?");
+
+var ctx = $('#canvas')[0].getContext("2d");
+//draw a circle
+ctx.beginPath();
+ctx.arc(75, 75, 10, 0, Math.PI*2, true); 
+ctx.closePath();
+ctx.fill();
+
+var x = 150;
+var y= 150;
+var dx = 2;
+var dy = 4;
+var ctx;
+
+function init() {
+  ctx = $$('#canvas') [0].getContext("2d");
+	return setInterval(draw, 10);
+	
+}
